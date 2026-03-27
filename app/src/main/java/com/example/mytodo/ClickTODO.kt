571f5d4af6a39d59cf9c225f6d0e9c2c4a3895a9
munir <munironi1997@gmail.com>
2026-03-27 17:05:26 +0500

    Навигатсия дохил кардам, дар экрани нав топбар ва тугмаи УДАЛИТЬро даровардам(хангоми пахши тугма аз экрани асоси ЗАДАЧИ 1 тоза мешавад) хамаи ЗАДАЧИ хоро ба массив таюдил додам ва ғайра...

---
+++ app/src/main/java/com/example/mytodo/ClickTODO.kt
@@ -0,0 +1,55 @@
+package com.example.mytodo
+
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun ClickTODO(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    items: MutableList<MainActivity.Less>
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Задача 1",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(paddingValues)
+        ){
+            Button (onClick = {
+            items.removeIf { it.lesSo == "Задача 1" }
+                navController.popBackStack()
+            },
+
+            ){
+             Text("Удалить")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -1,47 +1,148 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.mytodo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mytodo.ui.theme.MyTodoTheme
 
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             MyTodoTheme {
+                val lesSo = remember { mutableStateListOf(Less("Задача 1")) }
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = "TodoScreen",
                         modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("TodoScreen") {
+                            TodoScreen(navController = navController, items = lesSo)
+                        }
+                        composable("ClickTODO") {
+                            ClickTODO(navController = navController, items = lesSo)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun TodoScreen(modifier: Modifier = Modifier, navController: NavController,items: MutableList<Less>) {
+        Scaffold(
+            modifier = modifier,
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("TODO")
+                    }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = "Кнопка добавить"
+                    )
+                }
+            }
+        ) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                TodoItem(navController, items = items)
+            }
+        }
+    }
+
+    @Composable
+    private fun TodoItem(navController: NavController, items: MutableList<Less>,) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF28324F))
+        ) {
+            items(items.size) { index ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .clickable {
+                            navController.navigate("ClickTODO")
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Icon(Icons.Default.Delete, contentDescription = null)
+                    Text(text = items[index].lesSo, color = Color.White)
+                    Checkbox(
+                        checked = false,
+                        onCheckedChange = {
+
+                        }
                     )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTodoTheme {
-        Greeting("Android")
+    @Preview
+    @Composable
+    private fun TodoScreenPreview() = MyTodoTheme {
+        TodoScreen(
+            modifier = Modifier.fillMaxSize(),
+            navController = NavController(MainActivity()),
+            items = remember { mutableStateListOf(Less("Задача 1")) }
+        )
     }
+
+    data class Less(
+        var lesSo: String,
+    )
 }
