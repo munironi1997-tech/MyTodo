9db052709a7c7ea68a1d9b38527e97a29189c528
munir <munironi1997@gmail.com>
2026-03-28 23:46:58 +0500

    Добавлено обновление в чекбоксе

--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -26,12 +26,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -105,6 +107,10 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun TodoItem(navController: NavController, items: MutableList<Less>,) {
+        var checkboxPush by remember { mutableStateOf(false) }
+
+        val itemsColor = if (checkboxPush) Color.Red else Color.White
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
@@ -123,13 +129,14 @@ class MainActivity : ComponentActivity() {
 
                 ) {
                     Icon(Icons.Default.Delete, contentDescription = null)
-                    Text(text = items[index].lesSo, color = Color.White)
+                    Text(text = items[index].lesSo, color = itemsColor)
                     Checkbox(
-                        checked = false,
+                        checked = checkboxPush,
                         onCheckedChange = {
-
+                            checkboxPush = it
                         }
                     )
+
                 }
             }
         }
